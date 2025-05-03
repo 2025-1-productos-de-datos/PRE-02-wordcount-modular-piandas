87d9582344abee37a24f397bb2a556d563301f49
Santiago Bustos Pianda <119011448+piandas@users.noreply.github.com>
2025-05-03 10:58:59 -0500

    Agregar la función preprocess_lines para normalizar líneas de texto y actualizar la llamada en main()

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,4 +1,5 @@
 from homework.src._internals.count_words import count_words
+from homework.src._internals.preprocess_lines import preprocess_lines
 from homework.src._internals.read_all_lines import read_all_lines
 from homework.src._internals.split_in_words import split_in_words
 from homework.src._internals.write_count_words import write_count_words
@@ -6,16 +7,9 @@ from homework.src._internals.write_count_words import write_count_words
 
 def main():
 
-    ##
     all_lines = read_all_lines()
-
-    ## mover a "preprocess_lines"
-    all_lines = [line.lower().strip() for line in all_lines]
-
-    ## mover "split_in_words"
+    all_lines = preprocess_lines(all_lines)
     words = split_in_words(all_lines)
-
-    ## mover a "count_words"
     counter = count_words(words)
 
     # count the frequency of the words in the files in the input directory
