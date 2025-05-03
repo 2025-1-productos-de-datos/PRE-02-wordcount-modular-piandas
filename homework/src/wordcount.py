c541508e9114bfc4958cbb10d9542b7de9d9981e
Santiago Bustos Pianda <119011448+piandas@users.noreply.github.com>
2025-05-03 10:09:19 -0500

    Agregar la función write_count_words para guardar resultados en formato TSV y eliminar código redundante en wordcount.py

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,6 +1,8 @@
 # obtain a list of files in the input directory
 import os
 
+from homework.src.write_count_words import write_count_words
+
 
 def read_all_lines():
     all_lines = []
@@ -12,23 +14,11 @@ def read_all_lines():
     return all_lines
 
 
-def write_count_words(counter):
-    if not os.path.exists("data/output"):
-        os.makedirs("data/output")
-
-    # save the results using tsv format
-    with open("data/output/results.tsv", "w", encoding="utf-8") as f:
-        for key, value in counter.items():
-            # write the key and value to the file
-            f.write(f"{key}\t{value}\n")
-
-
 def main():
 
     all_lines = read_all_lines()
     input_file_list = os.listdir("data/input/")
 
-    # count the frequency of the words in the files in the input directory
     counter = {}
     for filename in input_file_list:
         with open("data/input/" + filename) as f:
@@ -37,7 +27,6 @@ def main():
                     w = w.lower().strip(",.!?")
                     counter[w] = counter.get(w, 0) + 1
 
-    # create the directory output/ if it doesn't exist
     write_count_words(counter)
 
 
