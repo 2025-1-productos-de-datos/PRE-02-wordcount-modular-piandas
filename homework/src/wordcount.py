ff4731b701ba2fc3b4ce670d934f5360d5a4ca37
Santiago Bustos Pianda <119011448+piandas@users.noreply.github.com>
2025-05-03 11:00:29 -0500

    separacion de funciones en archivos

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -11,17 +11,6 @@ def main():
     all_lines = preprocess_lines(all_lines)
     words = split_in_words(all_lines)
     counter = count_words(words)
-
-    # count the frequency of the words in the files in the input directory
-    # counter = {}
-    # for filename in input_file_list:
-    #     with open("data/input/" + filename) as f:
-    #         for l in f:
-    #             for w in l.split():
-    #                 w = w.lower().strip(",.!?")
-    #                 counter[w] = counter.get(w, 0) + 1
-
-    ##
     write_count_words(counter)
 
 
