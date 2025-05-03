fee37f7a0b2d8eeb96b1e64e5caff6b31926fecc
Santiago Bustos Pianda <119011448+piandas@users.noreply.github.com>
2025-05-03 10:55:41 -0500

    Refactor wordcount.py: mover funciones a módulos internos y simplificar el conteo de palabras

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,7 +1,6 @@
-# obtain a list of files in the input directory
-import os
-
+from homework.src._internals.count_words import count_words
 from homework.src._internals.read_all_lines import read_all_lines
+from homework.src._internals.split_in_words import split_in_words
 from homework.src._internals.write_count_words import write_count_words
 
 
@@ -14,14 +13,10 @@ def main():
     all_lines = [line.lower().strip() for line in all_lines]
 
     ## mover "split_in_words"
-    words = []
-    for line in all_lines:
-        words.extend(word.strip(",.!?") for word in line.split())
+    words = split_in_words(all_lines)
 
     ## mover a "count_words"
-    counter = {}
-    for word in words:
-        counter[word] = counter.get(word, 0) + 1
+    counter = count_words(words)
 
     # count the frequency of the words in the files in the input directory
     # counter = {}
